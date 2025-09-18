b41fa145ec9d718af680605dfd77ef651956e51a
lilianlacerda <lilianraiany@gmail.com>
2025-09-18 16:58:21 -0300

    add: Dia 2 do desafio. Criação da entity refeicao

---
+++ api/src/main/java/com/api/saude/api/entity/Refeicao.java
@@ -0,0 +1,24 @@
+package com.api.saude.api.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Refeicao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String tipo;
+    private Double quantidade;
+    private LocalDate data;
+    
+}
