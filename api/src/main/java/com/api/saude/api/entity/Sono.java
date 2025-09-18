d9fad10d638fce8b9c4d4b541b6734b71d6d6286
lilianlacerda <lilianraiany@gmail.com>
2025-09-18 16:58:28 -0300

    add: Dia 2 do desafio. Criação da entity sono

---
+++ api/src/main/java/com/api/saude/api/entity/Sono.java
@@ -0,0 +1,23 @@
+package com.api.saude.api.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Sono {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer horasDormidas;
+    private String qualidadeSono;
+    private LocalDate dataRegistro;
+    
+}
