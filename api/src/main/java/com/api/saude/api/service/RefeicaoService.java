40565947067d11a0c3e009e5b1399682b55f55c3
lilianlacerda <lilianraiany@gmail.com>
2025-09-19 17:21:44 -0300

    add: Dia 4 do desafio. Realizei o difierencial da criação da service de refeicao

---
+++ api/src/main/java/com/api/saude/api/service/RefeicaoService.java
@@ -0,0 +1,47 @@
+package com.api.saude.api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.api.saude.api.entity.Refeicao;
+import com.api.saude.api.repository.RefeicaoRepository;
+
+@Service
+public class RefeicaoService {
+    private final RefeicaoRepository refeicaoRepository;
+
+    public RefeicaoService (RefeicaoRepository refeicaoRepository){
+        this.refeicaoRepository = refeicaoRepository;
+    }
+
+    public List<Refeicao> listarRefeicao(){
+        return refeicaoRepository.findAll();
+    }
+
+    public List<Refeicao> cadastrarRefeicao(Refeicao refeicao){
+        refeicaoRepository.save(refeicao);
+        return listarRefeicao();
+    }
+
+    public Refeicao editarRefeicao(Long id, Refeicao refeicaoAtualizada){
+        Refeicao refeicao = refeicaoRepository.findById(id)
+                            .orElseThrow(() -> new RuntimeException("Refeição não encontrada pelo ID " + id));
+        
+        refeicao.setNome(refeicaoAtualizada.getNome());
+        refeicao.setTipo(refeicaoAtualizada.getTipo());
+        refeicao.setQuantidade(refeicaoAtualizada.getQuantidade());
+        refeicao.setData(refeicaoAtualizada.getData());
+        
+        return refeicaoRepository.save(refeicao);
+    }
+
+    public String deletarRefeicao (Long id){
+        if(!refeicaoRepository.existsById(id)){
+            return "Não foi possível localizar a refeição";
+        }
+
+        refeicaoRepository.deleteById(id);
+        return "Refeição excluida com sucesso" + listarRefeicao();
+    }
+}
