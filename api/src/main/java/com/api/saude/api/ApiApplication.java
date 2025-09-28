661c32cb8697954aa49bb8e5f91b100e94bfd2ea
lilianlacerda <lilianraiany@gmail.com>
2025-09-28 16:21:10 -0300

    add: layout da aplicação

--- api/src/main/java/com/api/saude/api/ApiApplication.java
+++ api/src/main/java/com/api/saude/api/ApiApplication.java
@@ -2,12 +2,20 @@ package com.api.saude.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
 	}
 
 }
