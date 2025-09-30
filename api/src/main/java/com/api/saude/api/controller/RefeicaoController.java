8066b158a44e4532a4034d49544615a407e7f09b
lilianlacerda <lilianraiany@gmail.com>
2025-09-30 19:33:32 -0300

    Mod: Mudanças nas reotas para integrar o thymeleaf

--- api/src/main/java/com/api/saude/api/controller/RefeicaoController.java
+++ api/src/main/java/com/api/saude/api/controller/RefeicaoController.java
@@ -1,21 +1,17 @@
 package com.api.saude.api.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.api.saude.api.entity.Refeicao;
 import com.api.saude.api.service.RefeicaoService;
 
-@RestController
+@Controller
 @RequestMapping("/refeicao")
 public class RefeicaoController {
 
@@ -26,26 +22,39 @@ public class RefeicaoController {
     }
 
     @GetMapping("/listar")
-    public ResponseEntity<List<Refeicao>> listarRefeicoes(){
-        List<Refeicao> refeicoes = refeicaoService.listarRefeicao();
-        return ResponseEntity.ok().body(refeicoes);
+    public String listarRefeicoes(Model model){
+        model.addAttribute("refeicoes", refeicaoService.listarRefeicao());
+        return "refeicao/refeicao";
+    }
+
+    @GetMapping("/cadastrar")
+    public String mostrarCadastro(Model model){
+        model.addAttribute("refeicao", new Refeicao());
+        return "refeicao/cadastrar";
     }
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<List<Refeicao>> cadastrarRefeicao(@RequestBody Refeicao refeicao){
-        List<Refeicao> refeicaoNova = refeicaoService.cadastrarRefeicao(refeicao);
-        return ResponseEntity.ok().body(refeicaoNova);
+    public String cadastrarRefeicao(@ModelAttribute Refeicao refeicao){
+        refeicaoService.cadastrarRefeicao(refeicao);
+        return "redirect:/refeicao/listar";
     }
 
-    @PutMapping("/editar/{id}")
-    public ResponseEntity<Refeicao> editarRefeicao (@PathVariable Long id, @RequestBody Refeicao refeicao){
-        refeicaoService.editarRefeicao(id, refeicao);
-        return ResponseEntity.ok().body(refeicao);
+    @GetMapping("/editar/{id}")
+    public String mostrarEdicao(@PathVariable Long id, Model model){
+        Refeicao refeicao = refeicaoService.buscarRefeicaoPorId(id);
+        model.addAttribute("refeicao", refeicao);
+        return "refeicao/editar";
     }
 
-    @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<Void> deletarRefeicao (@PathVariable Long id){
+    @PostMapping("/editar/{id}")
+    public String atualizarRefeicao (@PathVariable Long id, @ModelAttribute Refeicao refeicaoAtualizado){
+        refeicaoService.editarRefeicao(id, refeicaoAtualizado);
+        return "redirect:/refeicao/listar";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarRefeicao (@PathVariable Long id){
         refeicaoService.deletarRefeicao(id);
-        return ResponseEntity.noContent().build();
+        return "redirect:/refeicao/listar";
     }
 }
