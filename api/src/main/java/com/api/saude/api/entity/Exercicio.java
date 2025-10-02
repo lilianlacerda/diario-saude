e173df4797b6fcb35e0f57765f2f629dd10c02b1
lilianlacerda <lilianraiany@gmail.com>
2025-10-01 22:09:59 -0300

    mod: formato de data do projeto

--- api/src/main/java/com/api/saude/api/entity/Exercicio.java
+++ api/src/main/java/com/api/saude/api/entity/Exercicio.java
@@ -2,6 +2,8 @@ package com.api.saude.api.entity;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,10 +18,12 @@ public class Exercicio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDate data;
     private String nome;
     private Integer series;
     private Integer repeticoes;
     private Double carga;
     private Double tempo;
+
+    @DateTimeFormat(pattern = "yyy-MM-dd")
+    private LocalDate dataRegistro;
 }
